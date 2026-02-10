10c626a13735e7254273fe59b1620eb3af790d51
proyectos@nexatech.com.co <proyectos@nexatech.com.co>
2026-02-10 16:43:37 -0500

    cambios Revision Carlos Doria

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -35,7 +35,7 @@ const Home: React.FC = () => {
             {/* CAMBIO LÍNEA 34: Tipografía fluida aumentanda a md:text-6xl para tablets */}
             <h1 className="text-3xl md:text-5xl lg:text-7xl font-black mb-6 leading-[0.9] tracking-tighter">
               SERVICIOS DE {/* CAMBIO LÍNEA 35: Salto de línea controlado para móviles/tablets */} <br className="hidden sm:block" />
-              <span className="text-red-bright">INGENIERÍA,CONTRUCCION{/* CAMBIO LÍNEA 36: Salto de línea controlado para tablets */} <br className="hidden md:block" /> Y MANTENIMIENTO</span>
+              <span className="text-red-bright">INGENIERÍA, ,CONTRUCCION{/* CAMBIO LÍNEA 36: Salto de línea controlado para tablets */} <br className="hidden md:block" /> Y MANTENIMIENTO</span>
             </h1>
 
             {/* CAMBIO LÍNEA 39: Ancho máximo max-w-2xl para mejorar la lectura en pantallas anchas de tablets */}
